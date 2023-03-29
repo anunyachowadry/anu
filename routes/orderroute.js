86aba8037ed060ab0c44d0a5140c7f185acd6fc5
anunyachowdary <anunyachowdary125@gmail.com>
2023-03-29 14:49:50 +0530

    first commit

--- routes/orderroute.js
+++ routes/orderroute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router =express.Router();
 const mongoose = require('mongoose');
 
-const Order = require('../Models/order');
+const Order = require('../models/order');
 
 //Firebase initialization.
 
